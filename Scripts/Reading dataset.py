8018799d9d16f92b4178d009b7940ceb54dfc8cd
Joan Pujol Martinez GIT <joanpm1215@gmail.com>
2020-09-30 11:20:53 +0200

    Change on directory folders and tries in code

--- Scripts/Reading dataset.py
+++ Scripts/Reading dataset.py
@@ -1,14 +1,13 @@
 import pandas as pd
 import numpy as np
 
-df_traffic = pd.read_csv("Data-Collisions.csv")
+df_traffic = pd.read_csv("../data/Data-Collisions.csv")
+x = len(df_traffic.columns)
+pd.set_option("display.max_columns", x)
 df_traffic100 = df_traffic.head(100)
+print(df_traffic100.head())
+print(x)
 
-# print(df_traffic.columns)
-print(df_traffic["SEVERITYCODE"].value_counts())
-
-
-# print(df_traffic["ROADCOND"].isnull().sum())
 
 
 
